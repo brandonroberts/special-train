4e46177c5dddb714ed347d8074f06a44d0fb44dd
Brandon Roberts <robertsbt@gmail.com>
2017-08-06 13:27:27 -0500

    Added @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: []
+})
+export class FooModule {}
+
+export function root(state, action) {
+  return state;
+}
+
+export function reducer(state, action) {
+  return state;
+}
+
+@NgModule({
+  imports: [
+    StoreModule.forFeature('user', { reducer })
+  ]
+})
+export class UserModule {}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ root }),
+    UserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
